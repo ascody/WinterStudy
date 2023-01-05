86669a87effecdded7cda45f744033e2f56dee54
Ascody <54515519+ascody@users.noreply.github.com>
2023-01-05 13:48:57 +0900

    Delete chap_08 directory

--- chap_08/detector/Detectable.java
+++
@@ -1,5 +0,0 @@
-package chap_08.detector;
-public interface Detectable {
-    // public static final String name = "감지자"; == String name = "감지자";
-    public abstract void detect(); // 감지
-}
--- chap_08/reporter/Reportable.java
+++
@@ -1,4 +0,0 @@
-package chap_08.reporter;
-public interface Reportable {
-    void report(); // 신고
-}
