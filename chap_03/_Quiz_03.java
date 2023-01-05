37e73ae8517e3cbe72190f851c8c35088eb9a966
Ascody <54515519+ascody@users.noreply.github.com>
2023-01-05 13:49:40 +0900

    Delete chap_03 directory

--- chap_03/_Quiz_03.java
+++
@@ -1,9 +0,0 @@
-package chap_03;
-public class _Quiz_03 {
-    public static void main(String[] args) {
-        String s1 = "901231-1234567";
-        String s2 = "030708-4567890";
-        System.out.println(s1.substring(0, 8));
-        System.out.println(s2.substring(0, s2.indexOf('-')+2));
-    }
-}
